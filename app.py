d346ac42338621a5d18995d698666932116ceebb
B-LAB-DAAN <kalymbekbakdaulet@gmail.com>
2024-11-19 14:35:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ model.eval()
 
 preprocess = transforms.Compose([
     transforms.Grayscale(num_output_channels=3),
-    transforms.Resize((224, 224)),  # Resize for ResNet-18
+    transforms.Resize((224, 224)), 
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
 ])
